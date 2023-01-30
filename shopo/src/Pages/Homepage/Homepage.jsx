b4251e57c79fb6802eead692587900380f830ff0
afa1992 <afunaenigma34@gmail.com>
2023-01-30 11:03:01 +0400

    search box added

--- shopo/src/Pages/Homepage/Homepage.jsx
+++ shopo/src/Pages/Homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner/Banner";
+import Arrivals from "./Arrivals/Arrivals";
 
 const Homepage = () => {
   return (
     <div>
       <Banner />
+      <Arrivals />
     </div>
   );
 };
